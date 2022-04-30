cf22e36a6a8e755925f059e7501a19d6754722f9
cabrera-evil <douglascabrera.dev@gmail.com>
2022-04-30 17:51:43 -0600

    Update: dao

--- src/main/java/com/JavaWebsite/models/user.java
+++ src/main/java/com/JavaWebsite/models/user.java
@@ -1,60 +1,28 @@
 package com.JavaWebsite.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class user {
+
+    @Id @Getter @Setter @Column(name = "id")
     private Long id;
+    @Getter @Setter @Column(name = "name")
     private String name;
-    private String lastName;
+    @Getter @Setter @Column(name = "last_name")
+    private String last_name;
+    @Getter @Setter @Column(name = "email")
     private String email;
+    @Getter @Setter @Column(name = "phone")
     private String phone;
+    @Getter @Setter @Column(name = "password")
     private String password;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
-
-
